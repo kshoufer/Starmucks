1e4ad4ee7c97967e64243e8f2b31a3002a82db3b
ken shoufer <kshoufer@gmail.com>
2012-10-19 10:53:49 -0700

    Lower links
    added lower links for mobile

---
+++ starmucks.php
@@ -0,0 +1,91 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+
+<head>
+    <title>Starmucks Test Page1</title>
+	<link href="columnal.css" rel="stylesheet" type="text/css" />
+	<link href="custom.css?<?php echo time(); ?>" rel="stylesheet" type="text/css" />
+	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.css" />
+
+</head>
+<body>
+  <div id="wrapper" class="container">
+  <div id="header" class="row">
+
+    <div id="upper-navigation" class="col_4">
+
+        <ul>
+          <li><a href="#">Link1</a></li>
+          <li><a href="#">Link2</a></li>
+          <li><a href="#">Link3</a></li>
+        </ul>
+        
+        <input id="SearchText1" type="text" />
+
+    </div><!--end upper-navigation-->
+
+    <div id="upper-navigation-mobile" class="col_4">
+
+        <ul>
+          <li><a href="#">Link1</a></li>
+          <li><a href="#">Link3</a></li>
+        </ul>
+
+    </div><!--end upper-navigation-mobile-->
+
+    <div id="lower-navigation" class="row">    
+      
+      <div id="logo" class="col_2">
+        <img src="images/logo1.jpg" alt="" />
+      </div><!--end logo-->    
+  
+      <div id="nav2" class="col_4">  
+          <ul>
+          <li><a href="#">Link1</a></li>
+          <li><a href="#">Link2</a></li>
+          <li><a href="#">Link3</a></li>
+          <li><a href="#">Link4</a></li>
+          <li><a href="#">Link5</a></li>
+          <li><a href="#">Link6</a></li>
+          </ul>
+      </div>
+   
+    </div><!--end lower-navigation-->    
+  
+  </div><!--end header-->    
+
+
+  <div id="content" class="row">
+
+      <div id="main-image" class="col_2">
+        <img src="images/main-image1.jpg" alt="" />
+      </div><!--end logo-->    
+
+      <div id="nav3" class="col_4">  
+          <ul>
+          <li><a href="#">Link1</a></li>
+          <li><a href="#">Link2</a></li>
+          <li><a href="#">Link3</a></li>
+          <li><a href="#">Link4</a></li>
+          <li><a href="#">Link5</a></li>
+          <li><a href="#">Link6</a></li>
+          </ul>
+      </div>
+
+
+  </div><!--end content-->
+
+  <div id="sidebar">
+      sidebar
+  </div><!--end sidebar-->
+      footer
+
+  <div id="footer">
+  </div>
+
+  </div><!--end footer-->
+
+</body>
+</html>
